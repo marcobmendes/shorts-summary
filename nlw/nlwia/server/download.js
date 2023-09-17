fe1dd8f21da1d76748d3cafe289b0f6bd02429cc
Marco Antonio Brandini Mendes <137082335+marcobmendes@users.noreply.github.com>
2023-09-17 17:14:46 -0300

    Concluindo a Aula 2.

---
+++ nlw/nlwia/server/download.js
@@ -0,0 +1,23 @@
+import ytdl from "ytdl-core";
+import fs from 'fs'
+
+export const dowload = (videoId) => {
+  const videoURL = "https://www.youtube.com/shorts/" + videoId
+  // const videoURL = "https://www.youtube.com/watch?v=" + videoId
+  console.log("Realizando o download do vídeo...:", videoId)
+
+  ytdl(videoURL, {quality: "lowestaudio", filter: 'audioonly'})
+  .on("info", (info) =>{
+    const seconds = info.formats[0].approxDurationMs / 1000
+
+    if (seconds > 60){
+      throw new Error("A duração desse vídeo é maior do que 60 segundos.")
+    }
+  }
+  ).on("end", () => {
+    console.log("Download do vídeo finalizado.")
+  })
+  .on("error", (error) => {
+    console.log("Não foi possível fazer o dowload do vídeo. Detalhes do erro:", error)
+  }).pipe(fs.createWriteStream("./tmp/audio.mp4"))
+}
--- nlw/nlwia/server/index.js
+++ nlw/nlwia/server/index.js
@@ -1,11 +1,15 @@
 import cors from 'cors'
 import express from 'express'
 
+import { dowload } from './download.js'
+
 const app = express()
 app.use(cors())
 
-app.get('/summary', (request, response) => {
-  response.send("Hello World!")
+app.get('/summary/:id', (request, response) => {
+  dowload(request.params.id)
+  
+  response.json({result: "Dowload do vídeo realizado com sucesso!"})
 })
 
 app.listen(3333, () => console.log('Server is running on port 3333'))
