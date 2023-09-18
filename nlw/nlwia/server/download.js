1acbfd4e1345a1ce1a5429b3723ba1e1319d1ef6
Marco Antonio Brandini Mendes <137082335+marcobmendes@users.noreply.github.com>
2023-09-18 19:00:36 -0300

    2/3 da aula 3.

--- nlw/nlwia/server/download.js
+++ nlw/nlwia/server/download.js
@@ -1,23 +1,25 @@
 import ytdl from "ytdl-core";
 import fs from 'fs'
 
-export const dowload = (videoId) => {
+export const download = (videoId) => new Promise((resolve, reject) => {
   const videoURL = "https://www.youtube.com/shorts/" + videoId
   // const videoURL = "https://www.youtube.com/watch?v=" + videoId
   console.log("Realizando o download do vídeo...:", videoId)
 
   ytdl(videoURL, {quality: "lowestaudio", filter: 'audioonly'})
-  .on("info", (info) =>{
-    const seconds = info.formats[0].approxDurationMs / 1000
+    .on("info", (info) =>{
+      const seconds = info.formats[0].approxDurationMs / 1000
 
-    if (seconds > 60){
-      throw new Error("A duração desse vídeo é maior do que 60 segundos.")
+      if (seconds > 60){
+        throw new Error("A duração desse vídeo é maior do que 60 segundos.")
+      }
     }
-  }
-  ).on("end", () => {
-    console.log("Download do vídeo finalizado.")
-  })
-  .on("error", (error) => {
-    console.log("Não foi possível fazer o dowload do vídeo. Detalhes do erro:", error)
-  }).pipe(fs.createWriteStream("./tmp/audio.mp4"))
-}
+    ).on("end", () => {
+      console.log("Download do vídeo finalizado.")
+      resolve()
+    })
+    .on("error", (error) => {
+      console.log("Não foi possível fazer o dowload do vídeo. Detalhes do erro:", error)
+      reject(error)
+    }).pipe(fs.createWriteStream("./tmp/audio.mp4"))
+})
--- nlw/nlwia/server/index.js
+++ nlw/nlwia/server/index.js
@@ -1,15 +1,37 @@
 import cors from 'cors'
-import express from 'express'
+import express, { request } from 'express'
 
-import { dowload } from './download.js'
+import { convert } from "./convert.js"
+import { download } from './download.js'
+import { transcribe } from './transcribe.js'
+import { summarize } from "./summarize.js";
 
 const app = express()
+app.use(express.json())
 app.use(cors())
 
-app.get('/summary/:id', (request, response) => {
-  dowload(request.params.id)
-  
-  response.json({result: "Dowload do vídeo realizado com sucesso!"})
+app.get('/summary/:id', async (request, response) => {
+  try{
+  await download(request.params.id)
+  const audioConverted = await convert()
+  const result = await transcribe(audioConverted)
+
+  return response.json({ result })
+  } catch(error){
+    console.log(error)
+    return response.json({ error })
+  }
+})
+
+app.post("/summary", async (request, response) => {
+  try{
+  const result = await summarize(request.body.text)
+  return response.json({result})
+  }catch(error){
+    console.log(error)
+    return response.json({ error })
+  }
 })
 
 app.listen(3333, () => console.log('Server is running on port 3333'))
+
